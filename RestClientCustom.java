af6776c177c863b1e2ff2e7c27ecd137bc0cf096
vijaykumarrshingala <115060113+vijaykumarrshingala@users.noreply.github.com>
2025-05-12 19:08:47 +0530

    Update RestClientCustom.java

--- RestClientCustom.java
+++ RestClientCustom.java
@@ -1,7 +1,6 @@
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
@@ -25,16 +24,16 @@ public class RestClientProxyTruststoreApplication implements CommandLineRunner {
     private final int proxyPort = 8080;
     private final String proxyUsername = "your_proxy_username";
     private final String proxyPassword = "your_proxy_password";
-    private final String truststorePath = "classpath:your_truststore.jks"; // Place your JKS file in the resources folder
+    private final String truststorePath = "classpath:your_truststore.jks";
     private final String truststorePassword = "your_truststore_password";
-    private final String thirdPartyApiUrl = "https://api.example.com/some/endpoint"; // Replace with the actual API URL
+    private final String thirdPartyApiUrl = "https://api.example.com/some/endpoint";
 
     public static void main(String[] args) {
         SpringApplication.run(RestClientProxyTruststoreApplication.class, args);
     }
 
     @Bean
-    public RestClient restClient(RestClientBuilder builder) throws Exception {
+    public RestClient restClient() throws Exception {
         // 1. Configure Truststore
         KeyStore trustStore = KeyStore.getInstance("JKS");
         try (InputStream inputStream = getClass().getResourceAsStream(truststorePath)) {
@@ -55,10 +54,10 @@ public class RestClientProxyTruststoreApplication implements CommandLineRunner {
             return execution.execute(request, body);
         };
 
-        return builder
-                .requestFactory(() -> requestFactory)
-                .interceptors(Collections.singletonList(basicAuthInterceptor))
-                .defaultHeaders(headers -> headers.set("Accept", "application/json")) // Example header
+        return RestClient.builder()
+                .requestFactory(requestFactory)
+                .defaultHeaders(headers -> headers.set("Accept", "application/json"))
+                .interceptors(interceptors -> interceptors.add(basicAuthInterceptor))
                 .sslContext(sslContext)
                 .build();
     }
